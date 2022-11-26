41a33dab79708d573492a1f45d6f4a0e7d8eb8aa
Eh1z <godsentehiz11@gmail.com>
2022-11-26 17:42:05 +0100

    Final touches, already deployed build version

---
+++ src/constants/index.js
@@ -0,0 +1,199 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Rewards",
+    content:
+      "The best credit cards offer some tantalizing combinations of promotions and prizes",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "100% Secured",
+    content:
+      "We take proactive steps make sure your information and transactions are secure.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Balance Transfer",
+    content:
+      "A balance transfer credit card can save you a lot of money in interest charges.",
+  },
+];
+
+export const feedback = [
+  
+  {
+    id: "feedback-1",
+    content:
+      "Five years ago I \"switched\" to Blue Bank, I have been satisfied ever since. Not only has the bank met all my business and personal banking needs, but has done so with a willing, happy and energetic staff.",
+    name: "Eren Jaeger",
+    title: "Founder of \"The Jaegerists\"",
+    img: people03,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "I have worked with many banks and have found that working with Blue Bank was by far the most professional that I have ever had. I would encourage everyone to bank at Blue Bank.",
+    name: "Anos Voldigoad",
+    title: "Owner of MFT Academy",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "My experience with transitioning has been great. They make me feel that my business matters. I like that the bank still feels like a small town bank even though we are in quite a large community.",
+    name: "Naruto Uzumaki",
+    title: "The 7th Hokage",
+    img: people01,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Active Users",
+    value: "3800+",
+  },
+  {
+    id: "stats-2",
+    title: "Companies Trust Us",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Transactions",
+    value: "$230M+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.bluebank.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.bluebank.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.bluebank.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.bluebank.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.bluebank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.bluebank.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.bluebank.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.bluebank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.bluebank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.bluebank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.bluebank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.bluebank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
+
